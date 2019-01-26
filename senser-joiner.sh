f9cdc116f3eb515b4ca86fed0870743312c2181d
Seito Taka <amanoese@gmail.com>
2019-01-27 00:17:00 +0900

    second commit

--- senser-joiner.sh
+++ senser-joiner.sh
@@ -10,7 +10,8 @@ done
 cat temp |
   xargs -L1 -i sh -c 'cat {} | head -1 |sed "s/[^0-9]//g"' |
   sed -r -e 's/(....).*/\1/' -e 's/^$/0/' |
-  xargs -L3 | tr ' ' \\t | xargs -n3 |
-  sed -r 's/^0+//' |awk '{min=10000;for(n=1;n<NF;n+=1){if($n<min){min=$n}};print min}'
+  #xargs -L3 | tr ' ' \\t |
+  xargs -n1 |
+  sed -r 's/^0+//' |awk '{min=10000;for(n=1;n<=NF;n+=1){if($n<min){min=$n}};print min}'
 
 rm temp
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 
-./senser-joiner.sh <(cat -A /dev/urandom)  <(cat -A /dev/urandom)   <(cat -A /dev/urandom) <(cat -A /dev/urandom) <(cat -A /dev/urandom)  <(cat -A /dev/urandom)
+#./senser-joiner.sh <(cat -A /dev/urandom)  <(cat -A /dev/urandom)   <(cat -A /dev/urandom) <(cat -A /dev/urandom) <(cat -A /dev/urandom)  <(cat -A /dev/urandom)
+./senser-joiner.sh <(cat -A /dev/urandom)  <(cat -A /dev/urandom)
+#./senser-joiner.sh /dev/x1  /dev/x2  /dev/x3 /dev/y1  /dev/y2
